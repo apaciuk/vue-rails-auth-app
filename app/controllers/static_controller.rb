bc9db903c9df9ebd31816f50b55aa0feb83c1c9c
Paul Anthony McGowan <info@xhostcom.com>
2022-09-09 09:54:38 +0100

    User spec

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def home 
-    render json: { status: 200, message: "Static#home" }
+    render json: { status: 200, message: "Awesome" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do 
   it "is valid with valid attributes" do
-    user = User.new(email: "test@gmail.com")
+    user = User.new(email: "test@gmail.com", password: "123456")
     expect(user).to be_valid
   end
 end
