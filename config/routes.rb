5613af3e6314abbe361fabacb53711e91d7ff2e2
Paul Anthony McGowan <info@xhostcom.com>
2022-09-09 11:45:44 +0100

    User and sessions spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'registrations/create'
   get 'sessions/create'
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: %w[create]
+  resources :registrations, only: %w[create]
   root to: 'static#home'
 end
