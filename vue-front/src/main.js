bb2ed3b936b8633c681035ea53885cf02877b66d
Paul Anthony McGowan <info@xhostcom.com>
2022-09-10 12:05:15 +0100

    Axios plugin

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from './plugins/axios'
+
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(axios, {
+    baseUrl: 'http://localhost:3000/',
+})
 
 app.mount('#app')
